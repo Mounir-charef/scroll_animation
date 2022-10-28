57d9016964ede61e89469f72871b69b1fe26e5a5
Mounir <polomnr1@gmail.com>
2022-10-28 21:55:09 +0100

    finished test

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ const oberv = new IntersectionObserver((entries)=>{
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
+        // else {
+        //     entry.target.classList.remove('show');
+        // }
     })
 
 })
+console.log('haha');
 const hiddenElems = document.querySelectorAll('.hidden');
 hiddenElems.forEach((elem) => oberv.observe(elem));
